8bbad0121ec558a88b89e62a035724fb4aa0f2db
SKP <skp@skp.rocks>
2019-10-08 13:38:23 +0200

    ping pong

--- sendLumens.sh
+++ sendLumens.sh
@@ -3,7 +3,7 @@
 set receiver [lindex $argv 0]
 set message [lindex $argv 1]
 
-spawn keybase wallet send $receiver 1 XLM -m "$message"
+spawn keybase wallet send $receiver 0.0000001 XLM -m "$message"
 expect "(type 'YES' to confirm): "
 send -- "YES\r"
 expect eof
