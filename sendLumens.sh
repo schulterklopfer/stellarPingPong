7aaafb20be810fd318c86ed0bcbaa6e9fa8ef14f
SKP <skp@skp.rocks>
2019-10-08 13:28:07 +0200

    ping pong

--- sendLumens.sh
+++ sendLumens.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/expect -f
 
 set receiver [lindex $argv 0]
+set message [lindex $argv 1]
 
-spawn keybase wallet send $receiver 1 XLM
+spawn keybase wallet send $receiver 1 XLM -m "$message"
 expect "(type 'YES' to confirm): "
 send -- "YES\r"
 expect eof
--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env sh
+
+if [ ! $1 ]; then
+	echo "No poop receiver!"
+	exit
+fi
+
+pong=0
 while [ 1 -eq 1 ];
 do
-./sendLumens.sh $1
+message="💩 PING 💩"
+if [ $pong -eq 1 ]; then
+	pong=0
+	message="💩 PONG 💩"
+else
+	pong=1
+fi
+./sendLumens.sh $1 "$message"
 sleep 1
 done
